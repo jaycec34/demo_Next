32772013b7418823238a08338c4685495f7ff8bb
jayce cripe <jaycecripe@gmail.com>
2023-11-03 10:55:54 -0600

    Initial commit

---
+++ app/countButton.jsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react"
+
+export default function CountButton() {
+    let [count, setCount] = useState(0)
+    function handleClick() {
+        setCount(count + 1)
+    }
+    function rollOverCount() {
+        if (count > 10) {
+            setCount(0)
+        }
+    }
+    useEffect(rollOverCount, [count])
+    return (
+      <div>
+      <button onClick={handleClick}>+1</button>
+      <div>{count}</div>
+      </div>
+    )
+  }
