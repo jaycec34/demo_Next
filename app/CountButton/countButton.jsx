b13d52f989619c5c02e71a63cbe922d172b79515
jayce cripe <jaycecripe@gmail.com>
2023-11-04 15:52:36 -0600

    finished NextJS

--- app/CountButton/countButton.jsx
+++ app/CountButton/countButton.jsx
@@ -1,19 +1,25 @@
 import { useEffect, useState } from "react"
+import "./CountButton.css"
 
-export default function CountButton() {
+export default function CountButton(props) {
     let [count, setCount] = useState(0)
     function handleClick() {
-        setCount(count + 1)
+        setCount(count + 1 * props.mult)
     }
     function rollOverCount() {
         if (count > 10) {
             setCount(0)
         }
     }
+    const buttonStyle = {
+        backgroundColor: props.color
+
+    }
+
     useEffect(rollOverCount, [count])
     return (
       <div>
-      <button onClick={handleClick}>+1</button>
+      <button style={buttonStyle} onClick={handleClick}>{props.name}</button>
       <div>{count}</div>
       </div>
     )
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
 "use client"
-import CountButton from "./countButton"
-
+import CountButton from "./CountButton/countButton"
+import SearchBar from "./SearchBar/SearchBar"
 
 export default function Page() {
-    return <CountButton />
+    return <div>
+    <CountButton name={"+1"} mult={1} color={"green"}/><CountButton name="+2" mult={2} color={"yellow"}/>
+    <hr />
+    <a href="https://github.com/jaycec34/demo_Next">My Github Repo</a>
+    <hr />
+    <SearchBar />
+    </div>
   }
